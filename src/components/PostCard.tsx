4e9b38c0f60440098f3ed1cc50b284aa3e86ff43
min0k <mino.kim@minokim.com>
2022-03-07 15:07:49 -0800

    Create PostCard

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function PostCard() {
+  return (
+    <div>
+      <h1>Post Title</h1>
+      <h2>Author Name</h2>
+      <p>Post Text</p>
+      <p>Post Media</p>
+      <p># of Awards</p>
+      <a href="https://www.reddit.com">Link to post</a>
+    </div>
+  );
+}

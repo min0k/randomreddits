be325327a5d57dbe6702fdbb6f1b946b1d05fdc1
min0k <mino.kim@minokim.com>
2022-03-07 14:45:47 -0800

    Remove unneeded boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
 
 function App() {
-  // React.useEffect(() => {
-  //   async function getData() {
-  //     const res = await fetch("https://www.reddit.com/random.json");
-  //     const data = await res.json();
-  //     const post = data[0].data.children[0].data;
-  //     console.log(post);
-  //   }
-  // }, []);
+  React.useEffect(() => {
+    async function getData() {
+      try {
+        const res = await fetch("https://www.reddit.com/random.json");
+        const data = await res.json();
+        const post = data[0].data.children[0].data;
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;

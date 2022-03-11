8f950d90f5c2888972a475003a2935fdd242126e
min0k <mino.kim@minokim.com>
2022-03-11 10:22:13 -0800

    Add randomize feature
    While possibly not the best option, reloading the window seems to be the best way
    to implement a "randomize" feature with the current app architecture. There is no
    state that can trigger a re-render for {RandomReddits}.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import styles from "./App.module.css";
 import ReturnPostCard from "./components/ReturnPostCard";
+import React from "react";
 
-const NUMBER_OF_CARDS = 8;
+const NUMBER_OF_CARDS = 10;
+
+// TODO
+// Implmement Randomize feature. Force re-render?
 
 function App() {
   const RandomReddits = [...Array(NUMBER_OF_CARDS)].map((e, i) => (
     <ReturnPostCard key={i} />
   ));
 
-  return <div className={styles.app}>{RandomReddits}</div>;
+  console.log("Rerendered!");
+
+  return (
+    <div className={styles.app}>
+      <button
+        className={styles.randomizeButton}
+        onClick={() => window.location.reload()}
+      >
+        Randomize
+      </button>
+      <div className={styles.postContainer}>{RandomReddits}</div>
+    </div>
+  );
 }
 
 export default App;

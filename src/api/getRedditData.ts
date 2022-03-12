2dad8ec9b9f90ad88796aec732babeeb8a2d6839
min0k <mino.kim@minokim.com>
2022-03-11 21:46:03 -0800

    Add reddit URI as const

--- src/api/getRedditData.ts
+++ src/api/getRedditData.ts
@@ -1,6 +1,8 @@
+const URI = "https://www.reddit.com/random.json";
+
 export default async function getRedditData() {
   try {
-    const res = await fetch("https://www.reddit.com/random.json");
+    const res = await fetch(URI);
     const data = await res.json();
     const post = data[0].data.children[0].data;
     return post;

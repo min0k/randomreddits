43ce2b9417259b74cacbc2393b0355b42e9e2384
min0k <mino.kim@minokim.com>
2022-03-11 22:28:03 -0800

    Rewrite post data shape

--- src/api/getRedditData.ts
+++ src/api/getRedditData.ts
@@ -5,7 +5,7 @@ export default async function getRedditData() {
   try {
     const res = await fetch(TURI);
     const data = await res.json();
-    const post = data[0].data.children[0].data;
+    const post = data.data[0].data.children[0].data;
     return post;
   } catch (err) {
     console.log(err);
